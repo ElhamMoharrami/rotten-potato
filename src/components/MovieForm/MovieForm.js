2e48922dfce3aa1a24a193a38c388fea08b8bd40
ElhamMoharrami <elhammoharrami2001@gmail.com>
2022-10-06 19:09:10 +0330

    change movie form/add movie form validation

--- src/components/MovieForm/MovieForm.js
+++ src/components/MovieForm/MovieForm.js
@@ -8,15 +8,28 @@ import { useParams } from "react-router-dom";
 import Card from "../UI/Card/Card";
 import { useForm } from "react-hook-form";
 import classes from "./MovieForm.module.css";
+import { FormControl } from "@mui/material";
+import { InputLabel, Input, FormHelperText } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 
 const MovieForm = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-
   const navigate = useNavigate();
   const { register, reset, formState } = useForm();
   const isAddMode = !id;
+
+  const genreOptions = [
+    "action",
+    "thriller",
+    "drama",
+    "animation",
+    "romance",
+    "western",
+  ];
+  const languageOptions = ["English", "French", "Korean", "Farsi", "Turkish"];
 
   const movie = useSelector((state) => state.movies.selectedItem);
   const itemsPerPage = useSelector((state) => state.movies.data.itemsPerPage);
@@ -24,7 +37,17 @@ const MovieForm = () => {
   const currentYear = new Date().getFullYear();
 
   const [movieData, setMovieData] = useState({});
+
   const [urlIsValid, setUrlIsValid] = useState(true);
+  const [titleLengthIsValid, setTitleLengthIsValid] = useState(true);
+  const [rateNumberIsValid, setRateNumberIsValid] = useState(true);
+  const [imdbVotesIsValid, setImdbVotesIsValid] = useState(true);
+  const [runtimeIsValid, setRuntimeIsValid] = useState(true);
+  const [yearIsValid, setYearIsValid] = useState(true);
+  const [descriptiionIsValid, setDescriptionIsValid] = useState(true);
+  const [directorIsValid, setDirectorIsValid] = useState(true);
+  const [starsIsValid, setStarsIsValid] = useState(true);
+  const [awardsIsValid, setAwardsIsValid] = useState(true);
 
   const urlPatternValidation = (url) => {
     const regex = new RegExp(
@@ -33,6 +56,60 @@ const MovieForm = () => {
     const result = regex.test(url);
     if (!result) {
       setUrlIsValid(false);
+    }
+  };
+
+  const titleLengthValidation = (input) => {
+    if (input.length > 200) {
+      setTitleLengthIsValid(false);
+    }
+  };
+
+  const imdbRateValidation = (rate) => {
+    if (rate > 10 || rate < 0) {
+      setRateNumberIsValid(false);
+    }
+  };
+
+  const imdbVotesValidation = (votes) => {
+    if (votes.length > 8) {
+      setImdbVotesIsValid(false);
+    }
+  };
+
+  const runtimeValidation = (runtime) => {
+    if (runtime.length > 5) {
+      setRuntimeIsValid(false);
+    }
+  };
+
+  const yearValidation = (year) => {
+    if (year.length > 4) {
+      setYearIsValid(false);
+    }
+  };
+
+  const descriptionValidation = (input) => {
+    if (input.length > 500) {
+      setDescriptionIsValid(false);
+    }
+  };
+
+  const directorValidation = (director) => {
+    if (director.length > 100) {
+      setDirectorIsValid(false);
+    }
+  };
+
+  const starsValidation = (stars) => {
+    if (stars.length > 500) {
+      setStarsIsValid(false);
+    }
+  };
+
+  const awardsValidation = (awards) => {
+    if (awards.length > 500) {
+      setAwardsIsValid(false);
     }
   };
 
@@ -57,9 +134,35 @@ const MovieForm = () => {
       ...prevState,
       [name]: value,
     }));
-
     if (name === "poster") {
       urlPatternValidation(value);
+    }
+    if (name === "title") {
+      titleLengthValidation(value);
+    }
+    if (name === "imdbRating") {
+      imdbRateValidation(value);
+    }
+    if (name === "imdbVotes") {
+      imdbVotesValidation(value);
+    }
+    if (name === "runtime") {
+      runtimeValidation(value);
+    }
+    if (name === "year") {
+      yearValidation(value);
+    }
+    if (name === "description") {
+      descriptionValidation(value);
+    }
+    if (name === "director") {
+      directorValidation(value);
+    }
+    if (name === "actors") {
+      starsValidation(value);
+    }
+    if (name === "awards") {
+      awardsValidation(value);
     }
   };
 
@@ -109,6 +212,17 @@ const MovieForm = () => {
   }, [formState, movieData, reset]);
 
   const posterClasses = urlIsValid ? "data-form-input" : "invalid-input";
+  const formIsValid =
+    urlIsValid &&
+    titleLengthIsValid &&
+    rateNumberIsValid &&
+    imdbVotesIsValid &&
+    runtimeIsValid &&
+    yearIsValid &&
+    descriptiionIsValid &&
+    directorIsValid &&
+    starsIsValid &&
+    awardsIsValid;
 
   return (
     <div>
@@ -119,129 +233,245 @@ const MovieForm = () => {
             {isAddMode ? <p>add</p> : <p>edit</p>}{" "}
           </div>
           <div className={classes["data-form-input"]}>
-            <label>Movie name</label>
-            <input
-              type="text"
-              {...register("title")}
-              onChange={onchangeHandler}
-              value={movieData.title || ""}
-              required
-            />
+            <FormControl>
+              <InputLabel htmlFor="title-input">Movie Title</InputLabel>
+              <Input
+                error={titleLengthIsValid ? false : true}
+                {...register("title")}
+                onChange={onchangeHandler}
+                value={movieData.title || ""}
+                id="title-input"
+                aria-describedby="title-input"
+                required
+              />
+              {!titleLengthIsValid && (
+                <FormHelperText>
+                  should not be longer than 200 characters
+                </FormHelperText>
+              )}
+            </FormControl>
           </div>
           <div className={classes["data-form-input"]}>
-            <label>IMDB rating</label>
-            <input
-              type="number"
-              {...register("imdbRating")}
-              onChange={onchangeHandler}
-              value={movieData.imdbRating || ""}
-              step="0.1"
-              min="1"
-              max="10"
-            />
+            <FormControl>
+              <InputLabel htmlFor="imdbRating-input">IMDB rating</InputLabel>
+              <Input
+                type="number"
+                error={rateNumberIsValid ? false : true}
+                {...register("imdbRating")}
+                onChange={onchangeHandler}
+                value={movieData.imdbRating || ""}
+                id="imdbRating-input"
+                aria-describedby="imdbRating-input"
+                required
+              />
+              {!rateNumberIsValid && (
+                <FormHelperText>
+                  the number must be between 0 and 10
+                </FormHelperText>
+              )}
+            </FormControl>
           </div>
           <div className={classes["data-form-input"]}>
-            <label>IMDB votes</label>
-            <input
-              type="text"
-              {...register("imdbVotes")}
-              onChange={onchangeHandler}
-              value={movieData.imdbVotes || ""}
-            />
+            <FormControl>
+              <InputLabel htmlFor="imdbVotes-input">IMDB votes</InputLabel>
+              <Input
+                type="text"
+                error={imdbVotesIsValid ? false : true}
+                {...register("imdbVotes")}
+                onChange={onchangeHandler}
+                value={movieData.imdbVotes || ""}
+                id="imdbVotes-input"
+                aria-describedby="imdbVotes-input"
+              />
+              {!imdbVotesIsValid && (
+                <FormHelperText>
+                  should not be longer than 8 digits.
+                </FormHelperText>
+              )}
+            </FormControl>
           </div>
           <div className={classes["data-form-input"]}>
-            <label>Runtime</label>
-            <input
-              type="number"
-              {...register("runtime")}
-              onChange={onchangeHandler}
-              value={movieData.runtime || ""}
-            />
+            <FormControl>
+              <InputLabel htmlFor="runtime-input">Runtime</InputLabel>
+              <Input
+                type="text"
+                error={runtimeIsValid ? false : true}
+                {...register("runtime")}
+                onChange={onchangeHandler}
+                value={movieData.runtime || ""}
+                id="runtime-input"
+                aria-describedby="runtime-input"
+              />
+              {!runtimeIsValid && (
+                <FormHelperText>
+                  should not be longer than 5 digits.
+                </FormHelperText>
+              )}
+            </FormControl>
           </div>
           <div className={classes["data-form-input"]}>
-            <label>Year</label>
-            <input
-              type="number"
-              {...register("year")}
-              onChange={onchangeHandler}
-              value={movieData.year || ""}
-              required
-              min="1900"
-              max={currentYear}
-            />
+            <FormControl>
+              <InputLabel htmlFor="year-input">Year</InputLabel>
+              <Input
+                type="text"
+                error={yearIsValid ? false : true}
+                {...register("year")}
+                onChange={onchangeHandler}
+                value={movieData.year || ""}
+                min="1900"
+                max={currentYear}
+                id="year-input"
+                aria-describedby="year-input"
+                required
+              />
+              {!yearIsValid && (
+                <FormHelperText>
+                  should not be longer than 4 digits.
+                </FormHelperText>
+              )}
+            </FormControl>
           </div>
           <div className={classes["data-form-input"]}>
-            <label>Description</label>
-            <input
-              type="text"
-              {...register("plot")}
-              onChange={onchangeHandler}
-              value={movieData.plot || ""}
-            />
+            <FormControl>
+              <InputLabel htmlFor="plot-input">Description</InputLabel>
+              <Input
+                type="text"
+                error={descriptiionIsValid ? false : true}
+                {...register("plot")}
+                onChange={onchangeHandler}
+                value={movieData.plot || ""}
+                id="year-input"
+                aria-describedby="year-input"
+              />
+              {!descriptiionIsValid && (
+                <FormHelperText>
+                  should not be longer than 500 characters.
+                </FormHelperText>
+              )}
+            </FormControl>
           </div>
           <div className={classes["data-form-input"]}>
-            <label>Director</label>
-            <input
-              type="text"
-              {...register("director")}
-              onChange={onchangeHandler}
-              value={movieData.director || ""}
-              required
-            />
+            <FormControl>
+              <InputLabel htmlFor="director-input">Director</InputLabel>
+              <Input
+                type="text"
+                error={directorIsValid ? false : true}
+                {...register("director")}
+                onChange={onchangeHandler}
+                value={movieData.director || ""}
+                id="director-input"
+                aria-describedby="director-input"
+                required
+              />
+              {!directorIsValid && (
+                <FormHelperText>
+                  should not be longer than 100 characters.
+                </FormHelperText>
+              )}
+            </FormControl>
           </div>
           <div className={classes["data-form-input"]}>
-            <label>Stars</label>
-            <input
-              type="text"
-              {...register("actors")}
-              onChange={onchangeHandler}
-              value={movieData.actors || ""}
-            />
+            <FormControl>
+              <InputLabel htmlFor="actors-input">Stars</InputLabel>
+              <Input
+                type="text"
+                error={starsIsValid ? false : true}
+                {...register("actors")}
+                onChange={onchangeHandler}
+                value={movieData.actors || ""}
+                id="actors-input"
+                aria-describedby="actors-input"
+              />
+              {!starsIsValid && (
+                <FormHelperText>
+                  should not be longer than 500 characters.
+                </FormHelperText>
+              )}
+            </FormControl>
           </div>
           <div className={classes["data-form-input"]}>
-            <label>Generes</label>
-            <input
-              type="text"
-              {...register("genre")}
-              onChange={onchangeHandler}
-              value={movieData.genre || ""}
-              required
-            />
+            <FormControl>
+              <InputLabel id="genre-select">Genre</InputLabel>
+              <Select
+                labelId="genre-select"
+                {...register("genre")}
+                onChange={onchangeHandler}
+                value={movieData.genre || ""}
+                id="genre-select"
+                label="genre-selec"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                {genreOptions.map((genre, index) => (
+                  <MenuItem value={genre} key={index}>
+                    {genre}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </div>
           <div className={classes["data-form-input"]}>
-            <label>Languages</label>
-            <input
-              type="text"
-              {...register("language")}
-              onChange={onchangeHandler}
-              value={movieData.language || ""}
-              required
-            />
+            <FormControl>
+              <InputLabel id="language-select">Languages</InputLabel>
+              <Select
+                labelId="language-select"
+                {...register("language")}
+                onChange={onchangeHandler}
+                value={movieData.language || ""}
+                id="language-select"
+                label="language-selec"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                {languageOptions.map((language, index) => (
+                  <MenuItem value={language} key={index}>
+                    {language}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </div>
           <div className={classes["data-form-input"]}>
-            <label>Awards</label>
-            <input
-              type="text"
-              {...register("awards")}
-              onChange={onchangeHandler}
-              value={movieData.awards || ""}
-            />
+            <FormControl>
+              <InputLabel htmlFor="awards-input">Awards</InputLabel>
+              <Input
+                type="text"
+                error={awardsIsValid ? false : true}
+                {...register("awards")}
+                onChange={onchangeHandler}
+                value={movieData.awards || ""}
+                id="awards-input"
+                aria-describedby="awards-input"
+              />
+              {!awardsIsValid && (
+                <FormHelperText>
+                  should not be longer than 500 characters.
+                </FormHelperText>
+              )}
+            </FormControl>
           </div>
           <div className={classes["data-form-input"]}>
-            <label>Poster Url:</label>
-            <input
-              type="text"
-              {...register("poster")}
-              onChange={onchangeHandler}
-              value={movieData.poster || ""}
-              className={classes[`${posterClasses}`]}
-            />
+            <FormControl>
+              <InputLabel htmlFor="poster-input">Poster Url:</InputLabel>
+              <Input
+                type="text"
+                {...register("poster")}
+                onChange={onchangeHandler}
+                value={movieData.poster || ""}
+                className={classes[`${posterClasses}`]}
+                id="poster-input"
+                aria-describedby="poster-input"
+              />
+            </FormControl>
+
             {!urlIsValid && (
               <p className={classes["invalid-message"]}>invalid url</p>
             )}
           </div>
 
-          <Button disabled={urlIsValid ? false : true} type="submit">
+          <Button disabled={formIsValid ? false : true} type="submit">
             Submit
           </Button>
           <Button onClick={cancelHandler}>Cancel</Button>
