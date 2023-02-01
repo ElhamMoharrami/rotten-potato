91b0790cdb56b010eb0f6efea85b327b393b2a9f
ElhamMoharrami <103923973+ElhamMoharrami@users.noreply.github.com>
2023-02-01 11:08:03 +0330

    Update ReviewCard.js

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -1,31 +1,50 @@
 import React from "react";
-import InputAdornment from "@mui/material/InputAdornment";
 import Rating from "@mui/material/Rating";
-import Box from "@mui/material/Box";
-import OutlinedInput from "@mui/material/OutlinedInput";
+import ShowMoreText from "react-show-more-text";
+import { Card, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const ReviewCard = (props) => {
   const { data } = props;
 
   return (
-    <Box
-      sx={{
-        width: "100%",
-      }}
-    >
-      <OutlinedInput
-        fullWidth
-        inputProps={{ "data-testid": "review-comment" }}
-        id="user-review"
-        placeholder="Write Your Own Review"
-        value={data.description}
-        endAdornment={
-          <InputAdornment position="end">
+    <Card sx={{ boxShadow: 10, margin: "20px", padding: "10px" }}>
+      <Grid
+        container
+        spacing={3}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid xs={9} item>
+          <ShowMoreText
+            lines={2}
+            more="Show more"
+            less="Show less"
+            className="content-css"
+            anchorClass="show-more-less-clickable"
+            expanded={false}
+            truncatedEndingComponent={"... "}
+          >
+            <Typography>{data.description}</Typography>
+          </ShowMoreText>
+        </Grid>
+        <Grid xs={3} item>
+          <Item>
             <Rating readOnly value={data.rate} />
-          </InputAdornment>
-        }
-      />
-    </Box>
+          </Item>
+        </Grid>
+      </Grid>
+    </Card>
   );
 };
 
